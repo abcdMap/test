57541c8f4a7120a257e6b7a9d6c3fef6a2333cc8
soom <angelic572@daum.net>
2020-01-29 13:05:45 +0900

    init test

---
+++ src/main/kotlin/com/test/soom/example/SpringbootKotlinThymeleafApplication.kt
@@ -0,0 +1,11 @@
+package com.test.soom.example
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringbootKotlinThymeleafApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringbootKotlinThymeleafApplication>(*args)
+}
---
+++ src/test/kotlin/com/test/soom/example/SpringbootKotlinThymeleafApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.test.soom.example
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringbootKotlinThymeleafApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
